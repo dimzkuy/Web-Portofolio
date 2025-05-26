7821d1c2bc747273705811f37174bb05dc2dd2e3
Dimas Cahyo Margono <dimascahyomg855@gmail.com>
2025-05-26 23:54:13 +0700

    Replacing project to "src" directory

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,10 +20,13 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
